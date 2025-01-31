81984f288661e93b263c2a984a9dd365e2381421
Luis Morua <lfdomc@gmail.com>
2025-01-31 10:05:06 -0600

    commit19

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,27 @@ import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import GlobalContextProvider from './context/GlobalContext'
 import { BrowserRouter } from 'react-router-dom'
+import WhatsApp from './components/WhatsApp/WhatsApp'
 
 
 function App() {
-  
+
 
   return (
     <>
-    <BrowserRouter>
-    <GlobalContextProvider>
-    <Background>
-    <Header/>
-    <AppRoutes/>
-    <Footer/>
-    </Background>
-    </GlobalContextProvider>
-    </BrowserRouter> 
+      <BrowserRouter>
+        <GlobalContextProvider>
+          <Background>
+            <WhatsApp
+              imageSrc="/image/whatsapp.png"
+              onClick={() => window.location.href = "https://wa.me/50670120250"}
+            />
+            <Header />
+            <AppRoutes />
+            <Footer />
+          </Background>
+        </GlobalContextProvider>
+      </BrowserRouter>
     </>
   )
 }
